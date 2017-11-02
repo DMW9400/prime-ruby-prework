c02cef9ac576afa1f5700931f6093044cd8f709b
David Welch <dmw9400@gmail.com>
2017-11-02 19:15:02 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+def prime?(integer)
+  if integer <= 1
+    return false
+  elsif integer <= 3
+    return true
+  elsif integer%2 == 0 || integer%3 == 0
+    return false
+  p = 5
+  # while p * p <= n
+
+end
+
+# function is_prime(n)
+#     if n ≤ 1
+#         return false
+#     else if n ≤ 3
+#         return true
+#     else if n mod 2 = 0 or n mod 3 = 0
+#         return false
+#     let i ← 5
+#     while i * i ≤ n
+#         if n mod i = 0 or n mod (i + 2) = 0
+#             return false
+#         i ← i + 6
+#     return true
